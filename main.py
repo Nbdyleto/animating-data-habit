f67ce8ae29b1910484c09897daecb33fa94261fe
Nbdyleto <nobodyleto2004@gmail.com>
2022-05-30 23:12:27 -0300

    plotting data

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 # how to work better with dates? 
 may_days = [1 + i for i in range(10)]
-print(may_days)
 
 habits = [random.randint(0, 4) for i in range(len(may_days))]
 print(habits)
@@ -13,13 +12,12 @@ print(habits)
 plot_steps = np.array([habits[0]])
 last_habit = habits[0]
 
-4, 3
-
 for i in range(1, len(habits)):
     differences = habits[i] - last_habit 
     differences /= 50
-    for j in range(51):
-        plot_steps = np.append(plot_steps, [last_habit + differences * j], 0)
+    if differences != 0:
+        for j in range(51):
+            plot_steps = np.append(plot_steps, [last_habit + differences * j], 0)
     last_habit = habits[i]
 
 print(plot_steps)
