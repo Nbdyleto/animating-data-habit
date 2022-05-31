bab0bed43a54941cbcbe45087f87b277657c1960
Nbdyleto <nobodyleto2004@gmail.com>
2022-05-30 23:30:02 -0300

    trying to remove redundant data

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # how to work better with dates? 
 may_days = [1 + i for i in range(10)]
 
-habits = [random.randint(0, 4) for i in range(len(may_days))]
+habits = [random.randint(3, 4) for i in range(len(may_days))]
 print(habits)
 
 plot_steps = np.array([habits[0]])
@@ -15,9 +15,9 @@ last_habit = habits[0]
 for i in range(1, len(habits)):
     differences = habits[i] - last_habit 
     differences /= 50
-    if differences != 0:
-        for j in range(51):
-            plot_steps = np.append(plot_steps, [last_habit + differences * j], 0)
+    for j in range(51):
+        plot_steps = np.append(plot_steps, [last_habit + differences * j], 0)
+
     last_habit = habits[i]
 
 print(plot_steps)
@@ -36,5 +36,6 @@ for i, step in enumerate(plot_steps):
     plt.text(0.8, 0.2, f'day {day} is conclude {habits[day-1]} habits', fontsize=10)
     plt.pause(0.000000000000000000000000001)
     plt.clf()
+    #print(step)
 plt.show
 
