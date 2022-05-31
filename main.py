36395c9eea389cc2a37f25446e26c2b420212f31
Nbdyleto <nobodyleto2004@gmail.com>
2022-05-30 22:53:42 -0300

    change normal array to np.array

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import random
+import numpy as np
 
 # how to work better with dates? 
 may_days = [1 + i for i in range(30)]
@@ -8,7 +9,7 @@ print(may_days)
 habits = [random.randint(0, 4) for i in range(len(may_days))]
 print(habits)
 
-plot_steps = [habits[0]]
+plot_steps = np.array([habits[0]])
 last_habit = habits[0]
 
 4, 3
@@ -17,7 +18,7 @@ for i in range(1, len(habits)):
     differences = habits[i] - last_habit 
     differences /= 50
     for j in range(51):
-        plot_steps.append(last_habit + differences * j)
+        plot_steps = np.append(plot_steps, [last_habit + differences * j], 0)
     last_habit = habits[i]
 
 print(plot_steps)
