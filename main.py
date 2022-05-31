3be93e23aaefb80da6ccd15156f0726dab1b9f68
Nbdyleto <nobodyleto2004@gmail.com>
2022-05-30 23:11:35 -0300

    plotting data

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 # how to work better with dates? 
-may_days = [1 + i for i in range(30)]
+may_days = [1 + i for i in range(10)]
 print(may_days)
 
 habits = [random.randint(0, 4) for i in range(len(may_days))]
@@ -22,4 +23,20 @@ for i in range(1, len(habits)):
     last_habit = habits[i]
 
 print(plot_steps)
+
+# plotting:
+
+may_days = iter(may_days)
+day = next(may_days)
+
+for i, step in enumerate(plot_steps):
+    if i%50==0:
+        # i%50 represents that program passed for all steps of a transition
+        day = next(may_days)
+    plt.xlim(0,4)
+    plt.barh('May Habits', step)
+    plt.text(0.8, 0.2, f'day {day} is conclude {habits[day-1]} habits', fontsize=10)
+    plt.pause(0.000000000000000000000000001)
+    plt.clf()
+plt.show
 
